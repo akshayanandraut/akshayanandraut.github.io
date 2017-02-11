178a5789e1756b32c67b871c4d9cb6f5c83e8123
akshayanandraut <akshay.raut0303@gmail.com>
2017-02-11 23:42:08 +0530

    added couldnotconvert error

--- res/js_req.js
+++ res/js_req.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 	NProgress.start();
 	$('#loading').show();
 	$('.body').hide();	
+	$('.olcoder').hide();	
+//	$('.paytmTextBox').hide();	
 	
 });
 $(window).on('load',function() {	
@@ -19,10 +21,63 @@ $(window).scroll(function() {
     $('nav').removeClass('shrink');
   }
 });
-
 $(document).ready(function() {
     $('#projects_table').DataTable();
 } );
+
+$(document).ready(function(){
+	$('#olcoder').click(function(){		
+			$("#myModal .close").click();
+			$('.body1').hide("slow");
+			$('.nav').hide("slow");
+			$('.olcoder').show("slow");
+	});
+	/*$('#paytmTextBox').click(function(){		
+			$("#myModal .close").click();
+			$('.body1').hide("slow");
+			$('.nav').hide("slow");
+			$('.paytmTextBox').show("slow");
+	});*/
+});
+
+$(document).ready(function(){
+	$('.navbar-brand').click(function(){	
+		go_to_main();			
+	});
+		
+		
+			
+	
+});
+var changeFlag=false;
+function go_to_main()
+{
+	if(changeFlag)
+	{
+		if (confirm("Are you sure you want to leave your mess here? \n It will be automatically cleaned.")) {
+			$('.olcoder').hide("slow");		
+			$('.body1').show("slow");	
+			$('.nav').show("slow");
+			
+		} else {
+		   changeFlag=false;
+		}
+	}
+	else{
+		$('.olcoder').hide("slow");		
+		//$('.paytmTextBox').hide("slow");		
+			$('.body1').show("slow");	
+			$('.nav').show("slow");
+	}
+		
+	
+}
+
+function setChangeFlag()
+{
+	changeFlag = true;
+}
+
 
 
 $(window).scroll(function () {
@@ -41,3 +96,29 @@ $(window).scroll(function () {
         });	
     }
 });
+
+
+
+
+//PAYTM TEXTBOX  START
+/*
+$(document).ready(function (){
+
+	$('.inputText').focusout(function(){
+		if($(this).val() != ""){	
+			$('.username').addClass("anotherClass");		
+		}
+		else
+			$('.username').removeClass("anotherClass");
+	});
+	
+	$('.inputPass').focusout(function(){
+		if($(this).val() != ""){	
+			$('.password').addClass("anotherClass");		
+		}
+		else
+			$('.password').removeClass("anotherClass");
+	});
+});
+*/
+// PAYTM TEXTBOX END
