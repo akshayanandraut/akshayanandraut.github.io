089c1bf000aafc3e64256ce2bc54912268b972d1
Akshayanand Raut <akshay.raut0303@gmail.com>
2017-08-06 18:24:20 +0530

    added repo and follower label and back top top button

--- res/js_req.js
+++ res/js_req.js
@@ -1,4 +1,20 @@
-  
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        document.getElementById("backToTop").style.display = "block";
+    } else {
+        document.getElementById("backToTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 $(document).ready(function() {	
 	//$('.body').hide();	
 });
@@ -23,13 +39,13 @@ $(window).scroll(function () {
         $(".navbar").css({
             'height':'75px',
 			'padding-top':'10px',
-            'padding-bottom':'10px',			
+            'padding-bottom':'10px'
         });
     } else {
         $(".navbar").css({
             'height':'35px', 
 			'padding-top':'0px',
-            'padding-bottom':'4px',			
+            'padding-bottom':'4px'
         });	
     }
 });
